0bdc0c8119fdd365112081441efafa0f548b83a7
rtschu <robin.tschudi@gmail.com>
2022-08-11 15:18:00 +0200

    changed Plugins displayed name to be descriptive and in-line with conventions set by other report plugins

--- lang/de/scormreport_question.php
+++ lang/de/scormreport_question.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$string['pluginname'] = "Punkteverteilungskarte";
+$string['pluginname'] = "Einzelfragenauswertung";
 $string['report_scorm'] = "SCORM Auswertung";
 $string['report'] = "Auswertung";
 $string['report_for'] = "Auswertung für ";
